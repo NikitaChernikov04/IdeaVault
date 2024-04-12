fb54801369f67328d47d9e525e34ea247383bcf1
NikitaChernikov04 <113566014+NikitaChernikov04@users.noreply.github.com>
2024-04-12 18:26:40 +0700

    12.04.24

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from forms import RegistrationForm, LoginForm, ResetPasswordForm
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'my_secret_key_here'
+bootstrap = Bootstrap(app)
 
 @app.route('/main')
 @app.route('/')
@@ -11,17 +14,21 @@ def main():
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-        confirm_password = request.form['confirm_password']
+    form = RegistrationForm()
+    if request.method == 'POST' and form.validate:
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        confirm_password = form.confirm_password.data
+
+        if password != confirm_password:
+            return "Пароли не совпадают, попробуйте еще раз"
 
         # Здесь можно добавить логику для обработки данных, например, сохранение в базу данных
 
         return render_template('registration_success.html', username=username)
     else:
-        return render_template('registration.html')
+        return render_template('registration.html', form=form)
 
 
 @app.route('/success')
@@ -29,14 +36,20 @@ def success():
     return render_template('registration_success.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        return f"Email: {form.email.data}, Password: {form.password.data}"
+    return render_template('login.html', form=form)
 
 
-@app.route('/reset')
+@app.route('/reset', methods=['GET', 'POST'])
 def reset():
-    return render_template('reset_pass.html')
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        return f"Email: {form.email.data} - Запрос на восстановление пароля отправлен"
+    return render_template('reset_pass.html', form=form)
 
 
 if __name__ == '__main__':
